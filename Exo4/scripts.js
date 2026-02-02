837b93f09e3493c29f3259ee4380a8b6c20a4922
mael-donnadille <mael.donnadille@gmail.com>
2026-02-02 19:02:06 +0100

    Exo4 code corriger

---
+++ Exo4/scripts.js
@@ -0,0 +1,21 @@
+const cible = document.querySelector('h1#titre');
+cible.addEventListner(clic, 'action');
+function action( ) {
+ console.log(event.target);
+}
+
+
+// correction
+
+const cible = document.querySelector('h1#titre');
+
+cible.addEventListener("click", action);
+
+function action(event) {
+  console.log(event.target);
+}
+
+
+//PROBLEME: addEventListner -addEventListener 
+//manque guillemets autour de "click" 
+// La fonction action doit recevoir l'objet event en paramètre.
